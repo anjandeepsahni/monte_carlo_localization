d243a317f0947a4fc62b21b530c5996619fad564
Dhananjai Sharma <dhananjaisharma26@gmail.com>
2019-09-09 21:28:24 -0400

    Sensor model implementation

--- include/sensorModel.hh
+++ include/sensorModel.hh
@@ -5,16 +5,29 @@ using namespace std;
 class SensorModel
 {
     private:
-        
+        double z_hit;                           // coefficient for p_hit
+        double z_short;                         // coefficient for p_short
+        double z_max;                           // coefficient for p_max
+        double z_rand;                          // coefficient for p_rand
+        double max_range;                       // maximum range
+        vector<vector<double>> occupancy_map;   // occupancy map
 
     public:
-        SensorModel
-        {
-        }
+        SensorModel(double z_h,
+                    double z_s,
+                    double z_m,
+                    double z_r,
+                    double m_r,
+                    vector<vector<double>> o_m);
+        ~SensorModel();
 
-        ~SensorModel
-        {
-        }
+        /*
+         * beam_range_finder_model
+         * - z_t: range measurements
+         * - x_t1: predicted belief
+         */
+        double beam_range_finder_model(const vector<double> z_t1,
+                                       const vector<double> x_t1);
 
-        double beam_range_finder_model(vector<double>);
+        double ray_casting(void);
 };
--- src/sensorModel.cc
+++ src/sensorModel.cc
@@ -3,8 +3,34 @@
 using namespace std;
 
 
-
-double SensorModel::beam_range_finder_model(const vector<double>)
+SensorModel::SensorModel(double z_h, double z_s, double z_m, double z_r,
+                         double m_r, vector<vector<double>> o_m)
 {
+    z_hit = z_h;
+    z_short = z_s;
+    z_max = z_m;
+    z_rand = z_r;
+    max_range = m_r;
+    occupancy_map = o_m;
+}
 
+
+SensorModel::~SensorModel(void)
+{
+}
+
+
+double SensorModel::beam_range_finder_model(const vector<double> z_t1,
+                                            const vector<double> x_t1)
+{
+    double q = 1;
+    for (auto it: z_t1) {
+
+    }
+}
+
+double SensorModel::ray_casting(void)
+{
+    double z_true = 1;
+    return z_true;
 }
