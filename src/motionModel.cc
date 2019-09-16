daf209b5cd51413850958010bf67a8ea95ab454c
Dhananjai Sharma <dhananjaisharma26@gmail.com>
2019-09-16 16:53:52 -0400

    Fixed listing errors

--- src/motionModel.cc
+++ src/motionModel.cc
@@ -26,32 +26,37 @@ MotionModel::~MotionModel()
 
 state_t MotionModel::update(vector<double> u_t0, vector<double> u_t1, state_t x_t0)
 {
-    double del_rot1, del_trans, del_rot2, ddash_rot1, ddash_trans, ddash_rot2, term1, term2, term3;
+    double del_rot1, del_trans, del_rot2;
+    double ddash_rot1, ddash_trans, ddash_rot2;
+    double term1, term2, term3;
     
     // Simple trignometric operations to calculate the robots displacement <s,s'>
     del_rot1 = atan2(u_t1[1] - u_t0[1], u_t1[0] - u_t0[0]) - u_t0[2];
-    del_trans = sqrt(pow((u_t0[0]-u_t1[0]),2)+ pow((u_t0[1]-u_t1[1]),2));
-    del_rot2= u_t1[2]- u_t0[2]- del_rot1;
+    del_trans = sqrt(pow((u_t0[0] - u_t1[0]), 2) + pow((u_t0[1] - u_t1[1]), 2));
+    del_rot2 = u_t1[2] - u_t0[2] - del_rot1;
 
     // Standard deviation for each parmeter used in the next segment
-    term1 = alpha1*del_rot1 + alpha2*del_trans;
-    term2 = alpha3*del_trans + alpha4*(del_rot1 + del_rot2);
-    term3 = alpha1*del_rot2 + alpha2*del_trans;
+    term1 = alpha1 * del_rot1 + alpha2 * del_trans;
+    term2 = alpha3 * del_trans + alpha4 * (del_rot1 + del_rot2);
+    term3 = alpha1 * del_rot2 + alpha2 * del_trans;
 
     normal_distribution<double> d1{0, sqrt(term1)};
     normal_distribution<double> d2{0, sqrt(term2)};
     normal_distribution<double> d3{0, sqrt(term3)};
-    // Gaussian noise distribution with zero mean and sd for each paramter modelling the noise of each parameter
+
+    // Gaussian noise distribution with zero mean and sd for each paramter,
+    // thereby modelling the noise of each parameter
     random_device rd{};
     mt19937 gen{rd()};
-    ddash_rot1= del_rot1 - d1(gen);
-    ddash_trans= del_trans - d2(gen);
-    ddash_rot2= del_trans - d3(gen);
+    ddash_rot1 = del_rot1 - d1(gen);
+    ddash_trans = del_trans - d2(gen);
+    ddash_rot2 = del_trans - d3(gen);
 
-    // converting parameters from the robot's coordinate frame to the world coordinate frame, again by simple trignometric calculations. 
+    // Converting parameters from the robot's coordinate frame to the world
+    // coordinate frame, again by simpletrignometric calculations.
     state_t st;
-    st.x = x_t0.x + ddash_trans* cos(x_t0.theta + ddash_rot1);
-    st.y = x_t0.y + ddash_trans* sin(x_t0.theta + ddash_rot1);
+    st.x = x_t0.x + ddash_trans * cos(x_t0.theta + ddash_rot1);
+    st.y = x_t0.y + ddash_trans * sin(x_t0.theta + ddash_rot1);
     st.theta = x_t0.theta + ddash_rot1 + ddash_rot2;
     st.weight = 0.0;
     
@@ -103,10 +108,6 @@ int main(int argc, const char * argv[])
     // loading map and log file containing odometry data
     string src_path_map = "../data/map/wean.dat";
     string src_path_log = "../data/log/robotdata1.log";
-
-    // mapreader();
-    // Get occupancy map (Anjandeep)
-   
    
     // Calling motion model only
     MotionModel motion_model(0.1,0.1,0.1,0.1);
